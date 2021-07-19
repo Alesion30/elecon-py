cb885f998acef5401ff55610ce3ddc8379958aa3
Yuta Ohira <alesion90@gmail.com>
2021-07-20 03:44:08 +0900

    add: グラフを表示する関数を実装

--- src/controllers/graph.py
+++ src/controllers/graph.py
@@ -1,4 +1,5 @@
 from controllers.device import DeviceController
+import matplotlib.pyplot as plt
 import datetime
 
 
@@ -10,8 +11,10 @@ class DeviceGraphController():
         @params list[str] id_list デバイスID
         """
         self.id_list = id_list
-        self.devices: list[DeviceController] = list(map(lambda x: DeviceController(x), id_list)) # デバイスコントローラーリスト
-        self.datas = list(map(lambda x: x.get_device_id_data(start_at, end_at), self.devices)) # DBから取得したデータ
+        self.devices: list[DeviceController] = list(
+            map(lambda x: DeviceController(x), id_list))  # デバイスコントローラーリスト
+        self.datas = list(map(lambda x: x.get_device_id_data(
+            start_at, end_at), self.devices))  # DBから取得したデータ
 
     def get_id_list(self):
         """
@@ -25,3 +28,38 @@ class DeviceGraphController():
         # 重複削除
         list(set(id_list))
         return id_list
+
+    def show_graph(self, labels: list[str], colors: list[str]):
+        id_list = self.get_id_list()
+        for id in id_list:
+            # 各デバイスのグラフに必要なデータを格納
+            l = []
+
+            for data in self.datas:
+                if(id in data):
+                    item: list = data[id]
+                else:
+                    item = []
+
+                # x軸とy軸データ
+                rssi_list: list[int] = []
+                created_list: list[datetime] = []
+                for v in item:
+                    rssi_list.append(v['rssi'])
+                    created_list.append(v['created'])
+                l.append({'rssi': rssi_list, 'created': created_list})
+
+            # グラフ作成
+            fig = plt.figure()
+            plt.title(id)
+            plt.xlabel("time")
+            plt.ylabel("rssi")
+
+            for i, j in enumerate(l):
+                label = labels[i]
+                color = colors[i]
+                plt.plot(j['created'], j['rssi'], color=color, label=label)
+
+            plt.grid(True)
+            plt.legend()
+            plt.show()
--- src/main.py
+++ src/main.py
@@ -8,12 +8,15 @@ import datetime
 def main():
     fb_initialize_app()
 
-    start_at = datetime.datetime(2021, 7, 19, 17, 0, 0, tzinfo=tz_jst)
-    end_at = datetime.datetime(2021, 7, 19, 17, 1, 0, tzinfo=tz_jst)
+    start_at = datetime.datetime(2021, 7, 19, 17, 50, 0, tzinfo=tz_jst)
+    end_at = datetime.datetime(2021, 7, 19, 17, 51, 0, tzinfo=tz_jst)
 
-    controller = DeviceGraphController(['d742f58d5e3c5ef7'], start_at, end_at)
+    devices = ['0881269a1ac6746f', 'd742f58d5e3c5ef7']
+    labels = ['left', 'right']
+    colors = ['royalblue', 'orange']
 
-    data = controller.get_id_list()
-    print(data)
+    controller = DeviceGraphController(devices, start_at, end_at)
+    controller.show_graph(labels, colors)
+
 
 main()
